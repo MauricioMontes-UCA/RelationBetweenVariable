fe4942699a70e7c320567b6667dad33f9498f2ae
Mauricio Alejandro Montes Varela <128838142+MauricioMontes-UCA@users.noreply.github.com>
2023-06-06 22:30:43 +0000

    :sparles: Nueva estructura de datos

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+struct Comida
+{
+    int index;
+    string nombre;
+    float precio;
+};
+
 int main()
 {
-    //just adding a silly comment to cause a conflict
+    //Practicando un poco con struct
+    struct Comida producto1;
+    
+    producto1.index = 1;
+    producto1.nombre = "Hamburguesa";
+    producto1.precio = 2.75;
+
+    cout << producto1.index << endl;
+    cout << producto1.nombre << endl;
+    cout << producto1.precio << endl; 
+    
     return 0;
 }
