97ae1b33f2dee16a318946a798dd5513c0760261
MauricioMontes <00042823@uca.edu.sv>
2023-06-06 13:58:26 -0600

    :sparkles: El programa imprime los números del 1 al 10

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main()
 {
+    //Un simple programa para imprimir números del uno al diez
+    
+    int oneToTen[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << oneToTen[i] << endl;
+    }
     return 0;
 }
