c4e6f4f594b08da772658b71907ea6a3671e379d
Mauricio Alejandro Montes Varela <128838142+MauricioMontes-UCA@users.noreply.github.com>
2023-06-06 22:36:57 +0000

    :twisted_rightwards_arrows: Se han fusionado las ramas branch1 y branch2

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -12,14 +12,13 @@ struct Comida
 int main()
 {
     //Un simple programa para imprimir números del uno al diez
-    
     int oneToTen[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     for (int i = 0; i < 10; i++)
     {
         cout << oneToTen[i] << endl;
     }
-    
+
     //Practicando un poco con struct
     struct Comida producto1;
     
