e1dc2b98c4e48233ec82364938efb50f7afaa494
Mauricio Alejandro Montes Varela <128838142+MauricioMontes-UCA@users.noreply.github.com>
2023-09-02 21:22:49 -0600

    Adaptado Sumatorias.hpp para manejar paso por referencia

--- Sumatorias.hpp
+++ Sumatorias.hpp
@@ -1,52 +1,37 @@
 #include <iostream>
 
-double SumatoriaX(int n, double x[]); //Para la sumatoria de la variable independiente
-double SumatoriaXX(int n, double x[]); //Para la sumatoria de la variable dependiente
-double SumatoriaY(int n, double y[]); //Para la sumatoria de los cuadrados de la V.I
-double SumatoriaYY(int n, double y[]); //Para la sumatoria de los cuadrados de la V.D
-double SumatoriaXY(int n, double x[], double y[]); //Para la sumatoria del producto entre los pares ordendados
+void SumatoriaX(int n, double x[], double &sumX); //Para la sumatoria de la variable independiente
+void SumatoriaXX(int n, double x[], double &sumXX); //Para la sumatoria de la variable dependiente
+void SumatoriaY(int n, double y[], double &sumY); //Para la sumatoria de los cuadrados de la V.I
+void SumatoriaYY(int n, double y[], double &sumYY); //Para la sumatoria de los cuadrados de la V.D
+void SumatoriaXY(int n, double x[], double y[], double &sumXY); //Para la sumatoria del producto entre los pares ordendados
 
-double SumatoriaX(int n, double x[]){
-    double total = 0;
-
+void SumatoriaX(int n, double x[], double &sumX){
     for (int i = 0; i < n; i++){
-        total += x[i];
+        sumX += x[i];
     }
-    return total;
 }
 
-double SumatoriaXX(int n, double x[]){
-    double total = 0;
-
+void SumatoriaXX(int n, double x[], double &sumXX){
     for (int i = 0; i < n; i++){
-        total += (x[i] * x[i]);
+        sumXX += (x[i] * x[i]);
     }
-    return total;
 }
 
-double SumatoriaY(int n, double y[]){
-    double total = 0;
-
+void SumatoriaY(int n, double y[], double &sumY){
     for (int i = 0; i < n; i++){
-        total += y[i];
+        sumY += y[i];
     }
-    return total;
 }
 
-double SumatoriaYY(int n, double y[]){
-    double total = 0;
-
+void SumatoriaYY(int n, double y[], double &sumYY){
     for (int i = 0; i < n; i++){
-        total += (y[i] * y[i]);
+        sumYY += (y[i] * y[i]);
     }
-    return total;
 }
 
-double SumatoriaXY(int n, double x[], double y[]){
-    double total = 0;
-
+void SumatoriaXY(int n, double x[], double y[], double &sumXY){
     for (int i = 0; i < n; i++){
-        total += (x[i] * y[i]);
+        sumXY += (x[i] * y[i]);
     }
-    return total;
 }
